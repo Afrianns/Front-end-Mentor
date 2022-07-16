4c7d619131d972dbed65277ade5b263931896d9a
HanifNAfrian <hanifnafrian12@gmail.com>
2022-07-16 15:19:56 +0700

    minor fixed

--- intro-section-with-dropdown-navigation-main/src/main.js
+++ intro-section-with-dropdown-navigation-main/src/main.js
@@ -1,24 +1,21 @@
 
-const btn = document.querySelectorAll(".group");
-const menu = document.querySelectorAll(".show");
+const display = document.querySelectorAll(".show");
 const imgDrop = document.querySelectorAll(".img-drop");
-const dd = document.querySelector(".dropdown");
 const button = document.querySelectorAll(".btn");
 const Menu = document.querySelectorAll(".menu");
-const click = document.querySelectorAll(".click");
 
 
 function drop(n) {
 
-    menu[n].classList.toggle("hidden");
+    display[n].classList.toggle("hidden");
     imgDrop[n].classList.toggle("rotate-180");
     
 
     window.onclick = function(e){
             
         if(!e.target.matches(".drops")){
-            for (let i = 0; i < menu.length; i++) {
-                menu[i].classList.add("hidden");
+            for (let i = 0; i < display.length; i++) {
+                display[i].classList.add("hidden");
                 imgDrop[i].classList.remove("rotate-180");
                 
             }
@@ -28,18 +25,11 @@ function drop(n) {
 }
 
 
-
-
 button.forEach((s) => {
     s.addEventListener("click", () => {
-        // Menu.forEach((c) =>{
-            Menu[0].classList.toggle("translate-x-full")
-        
-        // e.classList.toggle("hidden")
-        // })
+        Menu[0].classList.toggle("translate-x-full")
 
-        // Menu[1].style.display = "block"
         Menu[1].classList.toggle("hidden")
-        Menu[1].classList.toggle("bg-opacity-80 hidden")
+        Menu[1].classList.toggle("opacity-80")
     })
 })
