9f12440af27e0845f11f7e3b2be9a3f414a63499
HanifNAfrian <hanifnafrian12@gmail.com>
2022-07-22 15:58:00 +0700

    fixed nav & mobile ver

--- Insure-Landing-Page/src/js/main.js
+++ Insure-Landing-Page/src/js/main.js
@@ -2,9 +2,11 @@
 const toggle = document.querySelector("#dropdown");
 const menu = document.querySelector(".dropdown-menu");
 const nav = document.querySelector(".content");
+const body = document.querySelector("body");
 
 toggle.addEventListener("click", function(){
     menu.classList.toggle("nav");
+    body.classList.toggle('nav-overflow');
     if(menu.classList.contains("nav")){
         toggle.style.backgroundImage = "url('../images/icon-close.svg')";
     } else{
