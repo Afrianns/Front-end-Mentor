d1f37a87db4430b3dac511e1987e4b634f243483
HanifNAfrian <hanifnafrian12@gmail.com>
2024-05-03 20:55:55 +0700

    fixed transition

--- faq-accordion/script/main.js
+++ faq-accordion/script/main.js
@@ -6,14 +6,14 @@ accordTitles.forEach((accordTitle, iteration) => {
   accordTitle.addEventListener("click", () => {
     hideAllDesc(iteration);
     indicators[iteration].classList.toggle("minus");
-    accordDescs[iteration].classList.toggle("hidden");
+    accordDescs[iteration].classList.toggle("close-gap");
   });
 });
 
 function hideAllDesc(current) {
   accordDescs.forEach((accordDesc, iteration) => {
-    if (!accordDesc.classList.contains("hidden") && current != iteration) {
-      accordDesc.classList.add("hidden");
+    if (accordDesc.classList.contains("close-gap") && current != iteration) {
+      accordDesc.classList.remove("close-gap");
       indicators[iteration].classList.remove("minus");
     }
   });
