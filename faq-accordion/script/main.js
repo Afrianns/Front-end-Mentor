fbc168cd461589ed92debaafd426b7f6a3ba7963
HanifNAfrian <hanifnafrian12@gmail.com>
2024-05-04 07:52:31 +0700

    fixed animation transition

--- faq-accordion/script/main.js
+++ faq-accordion/script/main.js
@@ -1,5 +1,5 @@
 let accordTitles = document.querySelectorAll("ul li h2");
-let accordDescs = document.querySelectorAll("ul li p");
+let accordDescs = document.querySelectorAll(".inside-wrapper");
 let indicators = document.querySelectorAll("span");
 
 accordTitles.forEach((accordTitle, iteration) => {
@@ -13,6 +13,7 @@ accordTitles.forEach((accordTitle, iteration) => {
 function hideAllDesc(current) {
   accordDescs.forEach((accordDesc, iteration) => {
     if (accordDesc.classList.contains("close-gap") && current != iteration) {
+      console.log("check " + iteration);
       accordDesc.classList.remove("close-gap");
       indicators[iteration].classList.remove("minus");
     }
